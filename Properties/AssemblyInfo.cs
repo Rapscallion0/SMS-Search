6dfefb70de7b2d4d0df8b765176022e4dcb28654
Kevin Sommers <kevin.sommers@locsoftware.com>
2026-01-23 08:38:42 -0700

    Bump version to 1.6.4.26023

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.2.26023")]
-[assembly: AssemblyFileVersion("1.6.2.26023")]
+[assembly: AssemblyVersion("1.6.4.26023")]
+[assembly: AssemblyFileVersion("1.6.4.26023")]
 
-[assembly: AssemblyInformationalVersion("1.6.2.26023")]
+[assembly: AssemblyInformationalVersion("1.6.4.26023")]
