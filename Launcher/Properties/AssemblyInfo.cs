9b54f20a45cf550d7b66091011ae77ebf3e1d2d5
Kevin Sommers <kevin.sommers@locsoftware.com>
2026-01-26 12:53:36 -0700

    Version alignment

--- Launcher/Properties/AssemblyInfo.cs
+++ Launcher/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.5.26023")]
-[assembly: AssemblyFileVersion("1.6.5.26023")]
-[assembly: AssemblyInformationalVersion("1.6.5.26023")]
+[assembly: AssemblyVersion("2.0.2.26026")]
+[assembly: AssemblyFileVersion("2.0.2.26026")]
+[assembly: AssemblyInformationalVersion("2.0.0.26023")]
